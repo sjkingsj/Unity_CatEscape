f539819ef545731dcaedcebc8729535fbf8d7729
sjkingsj <tmdwjd1210@naver.com>
2021-08-04 18:22:23 +0900

    CatEscape Assets

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Drop to constant velocity per frame
+        transform.Translate(0, -0.1f, 0);
+
+        // Destroy object when locate at outside of screen
+        if (transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+
+        // Collision Decision
+        Vector2 p1 = transform.position; // Center coordinate of Arrow
+        Vector2 p2 = this.player.transform.position; // Center coordinate of Player
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 0.5f; // Radius of Arrow
+        float r2 = 1.0f; // Radius of Player
+
+        if (d < r1 + r2)
+        {
+            // Transmit to Director Script that player and arrow collided
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().DecreaseHp();
+
+            // Remove the Arrow in the event of Collision
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // When input LArrow Key
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0); // Move left by 3
+        }
+
+        // When input RArrow Key
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0); // Move right by 3
+        }
+    }
+}
